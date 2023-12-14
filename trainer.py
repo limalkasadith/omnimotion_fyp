f7045ccc91ec7609ec86b8f1c9d3c2e51b877f91
Induwara Senadheera <755pansilusenadheera@gmail.com>
2023-12-14 22:54:27 +0530

    change optical flow weight

--- trainer.py
+++ trainer.py
@@ -528,7 +528,7 @@ class BaseTrainer():
         canonical_unit_sphere_loss = self.canonical_sphere_loss(x1s_canonical_samples)
         # print("divloss",div_loss)
 
-        loss = 0*optical_flow_loss + \
+        loss = optical_flow_loss + \
                w_rgb * (loss_rgb + loss_rgb_grad) + \
                w_depth_range * depth_range_loss + \
                w_distortion * distortion_loss + \
